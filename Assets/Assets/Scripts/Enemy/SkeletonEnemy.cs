d1d30281fd40af7ee5c5cce08e66cab70e0b667b
Xavian <156493129+xzillagame@users.noreply.github.com>
2024-04-09 16:47:23 -0500

    Basic Enemy State Machine

--- Assets/Assets/Scripts/Enemy/SkeletonEnemy.cs
+++ Assets/Assets/Scripts/Enemy/SkeletonEnemy.cs
@@ -9,15 +9,32 @@ public class SkeletonEnemy : MonoBehaviour
     [SerializeField] private Animator EnemyAnimator;
 
 
-    [SerializeField] private PlayerController Player;
+    private PlayerController Player;
 
-    private bool PlayerFound = false;
+
+    private Vector3 StartingPosition;
+
+
+    public enum EnemyState
+    {
+        Idle,
+        Chase,
+        Return,
+        Attack
+    }
+    public EnemyState SkeletonState;
+
+
+
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartingPosition = transform.position;  
+        SkeletonState = EnemyState.Idle;
+        Player = null;
     }
 
     // Update is called once per frame
@@ -25,15 +42,105 @@ public class SkeletonEnemy : MonoBehaviour
     {
 
 
-        if(PlayerFound == true)
+        switch (SkeletonState)
         {
-            EnemyNavAgent.SetDestination(Player.transform.position);
-            EnemyAnimator.SetBool("isMoving", true);
+            case EnemyState.Idle:
+                IdleUpdate();
+                break;
+
+            case EnemyState.Chase:
+                ChaseUpdate();
+                break ;
+
+            case EnemyState.Return:
+                ReturnUpdate();
+                break;
+
+            case EnemyState.Attack:
+                StartCoroutine(AttackUpdate());
+                break;
+
+            default:
+                break;
+
+                    
         }
 
 
     }
 
+
+
+    private void IdleUpdate()
+    {
+        EnemyAnimator.SetBool("isMoving", false);
+    }
+
+
+    private void ChaseUpdate()
+    {
+
+        if(Player == null)
+        {
+            SkeletonState = EnemyState.Return;
+            return;
+        }
+
+
+
+        Vector3 playerPosition = Player.transform.position;
+        playerPosition.y = 0f;
+
+        EnemyNavAgent.SetDestination(playerPosition);
+        EnemyAnimator.SetBool("isMoving", true);
+
+
+        float distanceToPlayer = Vector3.Distance(Player.transform.position, transform.position);
+
+        if(distanceToPlayer < 2f)
+        {
+            SkeletonState = EnemyState.Attack;
+        }
+
+
+    }
+
+    private void ReturnUpdate()
+    {
+
+        float distance = Vector3.Distance(StartingPosition,transform.position);
+
+        if (distance < 0.5f)
+        {
+            Debug.Log("position reached");
+            SkeletonState = EnemyState.Idle;
+        }
+
+    }
+
+    private IEnumerator AttackUpdate()
+    {
+
+        EnemyAnimator.Play("DS_onehand_attack_A");
+
+
+        EnemyNavAgent.isStopped = true;
+
+        Debug.Log("Calling wait");
+
+
+        float animLength = EnemyAnimator.GetCurrentAnimatorStateInfo(0).length;
+
+
+        yield return new WaitForSeconds(animLength);
+        Debug.Log("After wait");
+
+        EnemyNavAgent.isStopped = false;
+
+        SkeletonState = EnemyState.Chase;
+
+
+    }
 
 
 
@@ -41,18 +148,28 @@ public class SkeletonEnemy : MonoBehaviour
     {
         if(other.TryGetComponent<PlayerController>(out PlayerController player))
         {
+            //Set State to Chase Player
+
+            SkeletonState = EnemyState.Chase;
             Player = player;
-            PlayerFound = true;
+
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if( other.GetComponent<PlayerController>() != null )
+        if (other.GetComponent<PlayerController>() != null)
         {
+
+            //Set State to Return
+
+            SkeletonState = EnemyState.Return;
+
+            EnemyNavAgent.SetDestination(StartingPosition);
+            EnemyAnimator.SetBool("isMoving", true);
+
             Player = null;
-            PlayerFound = false;
-            EnemyAnimator.SetBool("isMoving", false);
+
         }
     }
 
